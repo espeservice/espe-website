73c2d9861b3bb3c42aa6b2bbe0489196a8e9e270
chrisoemma <chrissoemma@gmail.com>
2024-12-05 21:21:48 +0300

    Added logo and fav icon

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between p-4 shadow-sm">
       <div className="flex items-center gap-10">
-        <Image src="/logo.png" alt="espe logo" width={100} height={50} />
+        <Image src="/logo.png" alt="espe logo" width={147} height={55} />
         <ul className="md:flex gap-10 hidden">
           {menu.map((item, index) => (
             <Link href={item.path}>
